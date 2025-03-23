65a4ced061362a8f0843be8a1b34f552d0a7665a
balugaq <129668183+balugaq@users.noreply.github.com>
2025-03-23 10:27:04 +0800

    Update Models.java

--- src/main/java/com/balugaq/jeg/utils/Models.java
+++ src/main/java/com/balugaq/jeg/utils/Models.java
@@ -7,14 +7,7 @@ import org.bukkit.inventory.ItemStack;
 
 @Deprecated
 public class Models {
-    public static final ItemStack RTS_ITEM = Converter.getItem(new SlimefunItemStack("_UI_RTS_ICON", Converter.getItem(Material.ANVIL, "&b实时搜索", "")));
-    public static final ItemStack SPECIAL_MENU_ITEM = Converter.getItem(new SlimefunItemStack("_UI_SPECIAL_MENU_ICON", Converter.getItem(Material.COMPASS, "&b超大配方", "", "&a点击打开超大配方(若有)")));
-    public static final ItemStack INPUT_TEXT_ICON = Converter.getItem(new SlimefunItemStack("_UI_RTS_INPUT_TEXT_ICON", Converter.getItem(
-            Material.PAPER,
-            "&f搜索: &7在上方输入搜索词",
-            "&fNote:",
-            "&7 - &e左侧物品为返回键",
-            "&7 - &e中间物品为按键上一页",
-            "&7 - &e右侧物品为按键下一页"
-    )));
+    public static final ItemStack RTS_ITEM = Converter.getItem(new SlimefunItemStack("_UI_RTS_ICON", Converter.getItem(Material.ANVIL, "Deprecated Icon")));
+    public static final ItemStack SPECIAL_MENU_ITEM = Converter.getItem(new SlimefunItemStack("_UI_SPECIAL_MENU_ICON", Converter.getItem(Material.COMPASS, "Deprecated Icon")));
+    public static final ItemStack INPUT_TEXT_ICON = Converter.getItem(new SlimefunItemStack("_UI_RTS_INPUT_TEXT_ICON", Converter.getItem(Material.PAPER, "Deprecated Icon")));
 }
