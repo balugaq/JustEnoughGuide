04b3203052aa57747398b69f26eb5e8e10e5f24a
balugaq <129668183+balugaq@users.noreply.github.com>
2026-07-21 20:22:33 +0800

    update license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2024-2026 balugaq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 plugins {
     java
     id("com.gradleup.shadow") version "9.0.0"
