0af66b1e252bc327a04b76865609ae2bd1855226
balugaq <129668183+balugaq@users.noreply.github.com>
2026-08-20 20:41:04 +0800

    fix cannot shift-interact in backpack

--- src/main/java/com/balugaq/jeg/core/listeners/MenuListener.java
+++ src/main/java/com/balugaq/jeg/core/listeners/MenuListener.java
@@ -17,7 +17,9 @@
 
 package com.balugaq.jeg.core.listeners;
 
+import city.norain.slimefun4.api.menu.UniversalMenu;
 import city.norain.slimefun4.holder.SlimefunInventoryHolder;
+import io.github.thebusybiscuit.slimefun4.api.player.PlayerBackpack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -25,6 +27,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 public class MenuListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -34,7 +37,7 @@ public class MenuListener implements Listener {
             || event.getClick() == ClickType.SHIFT_RIGHT)
             && event.getRawSlot() >= event.getInventory().getSize()) {
             var holder = event.getInventory().getHolder();
-            if (holder instanceof SlimefunInventoryHolder && !(holder instanceof BlockMenu)) {
+            if (isBlacklistedHolder(holder)) {
                 event.setCancelled(true);
             }
         }
@@ -44,9 +47,17 @@ public class MenuListener implements Listener {
     public void onMenuDrag(InventoryDragEvent event) {
         if (event.getRawSlots().stream().mapToInt(i -> i).max().orElse(0) < event.getInventory().getSize()) {
             var holder = event.getInventory().getHolder();
-            if (holder instanceof SlimefunInventoryHolder && !(holder instanceof BlockMenu)) {
+            if (isBlacklistedHolder(holder)) {
                 event.setCancelled(true);
             }
         }
+    }
+
+    public static boolean isBlacklistedHolder(InventoryHolder holder) {
+        // filter out guide menus
+        return holder instanceof SlimefunInventoryHolder
+            && !(holder instanceof BlockMenu)
+            && !(holder instanceof UniversalMenu)
+            && !(holder instanceof PlayerBackpack);
     }
 }
